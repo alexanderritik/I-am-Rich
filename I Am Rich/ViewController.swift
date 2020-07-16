8fd822c503fd69ab450e06999ada02cdc01af885
Ritik <rakeshkusrivastav@gmail.com>
2020-07-17 00:19:20 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ritik Srivastava on 17/07/20.
+//  Copyright © 2020 Ritik Srivastava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
